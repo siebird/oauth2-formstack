9e486775d7720889cc83e817a922adca03d34a09
Austin Siewert <austin@siebird.com>
2016-11-14 17:22:17 -0500

    Added resource owner scripts
    Signed-off-by: Austin Siewert <austin@siebird.com>

--- src/Provider/Formstack.php
+++ src/Provider/Formstack.php
@@ -2,10 +2,8 @@
 
 namespace Siebird\OAuth2\Client\Provider;
 
-use GuzzleHttp\Psr7\Uri;
 use League\OAuth2\Client\Provider\AbstractProvider;
-use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
-use League\OAuth2\Client\Provider\ResourceOwnerInterface;
+use Siebird\OAuth2\Client\Provider\Exception\FormstackIdentityProviderException;
 use League\OAuth2\Client\Token\AccessToken;
 use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
 use Psr\Http\Message\ResponseInterface;
@@ -14,49 +12,113 @@ class Formstack extends AbstractProvider
 {
     use BearerAuthorizationTrait;
 
+    /**
+     * Domain
+     *
+     * @var string
+     */
+    public $domain = 'https://www.formstack.com';
+
+    /**
+     * Api domain
+     *
+     * @var string
+     */
+    public $apiDomain = 'https://www.formstack.com';
+
+    /**
+     * Get authorization url to begin OAuth flow
+     *
+     * @return string
+     */
     public function getBaseAuthorizationUrl()
     {
-        return 'https://www.formstack.com/api/v2/oauth2/authorize';
+        return $this->domain.'/api/v2/oauth2/authorize';
     }
 
+    /**
+     * Get access token url to retrieve token
+     *
+     * @param  array $params
+     *
+     * @return string
+     */
     public function getBaseAccessTokenUrl(array $params)
     {
-        return 'https://www.formstack.com/api/v2/oauth2/token';
+        return $this->domain.'/api/v2/oauth2/token';
     }
 
+    /**
+     * Get provider url to fetch user details
+     *
+     * @param  AccessToken $token
+     *
+     * @return string
+     */
     public function getResourceOwnerDetailsUrl(AccessToken $token)
     {
-        return [];
+        if ($this->domain === 'https://api.harvestapp.com') {
+            return $this->apiDomain.'/account/who_am_i';
+        }
+        return $this->domain.'/account/who_am_i';
     }
 
+    /**
+     * Get the default scopes used by this provider.
+     *
+     * This should not be a complete list of all scopes, but the minimum
+     * required for the provider user interface!
+     *
+     * @return array
+     */
     protected function getDefaultScopes()
     {
         return [];
     }
 
+    /**
+     * Check a provider response for errors.
+     *
+     * @throws FormstackIdentityProviderException
+     * @param  ResponseInterface $response
+     * @param  string $data Parsed response data
+     * @return void
+     */
     protected function checkResponse(ResponseInterface $response, $data)
     {
         if ($response->getStatusCode() >= 400) {
-            $msg = $data['error_description'] ?: (string)$response->getReasonPhrase();
-            throw new IdentityProviderException(
-                $msg,
-                $response->getStatusCode(),
-                $response
-            );
+            throw FormstackIdentityProviderException::clientException($response, $data);
+        } elseif (isset($data['error'])) {
+            throw FormstackIdentityProviderException::oauthException($response, $data);
         }
     }
 
+    /**
+     * Generate a user object from a successful user details request.
+     *
+     * @param array $response
+     * @param AccessToken $token
+     * @return League\OAuth2\Client\Provider\ResourceOwnerInterface
+     */
     protected function createResourceOwner(array $response, AccessToken $token)
     {
-        return new FormstackResourceOwner($response);
+        $user = new FormstackResourceOwner($response);
+
+        return $user->setDomain($this->domain);
     }
 
-    protected function fetchResourceOwnerDetails(AccessToken $token)
+    /**
+     * Returns an authenticated PSR-7 request instance.
+     *
+     * @param  string $method
+     * @param  string $url
+     * @param  AccessToken|string $token
+     * @param  array $options Any of "headers", "body", and "protocolVersion".
+     * @return RequestInterface
+     */
+    public function getAuthenticatedRequest($method, $url, $token, array $options = [])
     {
-        $url = $this->getResourceOwnerDetailsUrl($token);
-
-        $request = $this->getAuthenticatedRequest(self::METHOD_POST, $url, $token);
-
-        return $this->getResponse($request);
+        $options['headers'] = array('Content-Type' => 'application/json','Accept' => 'application/json');
+        return $this->createRequest($method, $url, $token, $options);
     }
 }
